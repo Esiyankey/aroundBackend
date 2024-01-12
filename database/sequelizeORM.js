e65bfb3a1776a15b94659d567e56e33ea382dd3b
Esi Yankey <graceyan244@gmail.com>
2024-01-12 13:55:54 +0000

    connecting the database

---
+++ database/sequelizeORM.js
@@ -0,0 +1,23 @@
+const Sequelize = require ("sequelize")
+
+
+// creating a connection to the postgres Database
+
+const sequelize = new Sequelize('aroundDatabase','postgres','password'{
+    host:' localhost',
+    dialect:'postgres'
+})
+
+
+// using sequelize .authenticate to establish the connection and logging whether it was successfull or not 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("DB Connection successful");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+  module.exports={sequelize}
