59bf3f3a60bc14ba41ebb2bedd8ae263a0889acc
Esi Yankey <graceyan244@gmail.com>
2024-01-12 14:29:44 +0000

    installed some few packages

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const express = require("express")
-const app = express()
-const userRouter = require("./routes/userRouter")
-const port = "5000"
+const express = require("express");
+const app = express();
+const userRouter = require("./routes/userRouter");
+const {sequelize} = require("./database/sequelizeORM");
+const port = "5000";
 
-app.use("/user",userRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/user", userRouter);
 
-app.listen(port,()=>{
-    console.log(`server running on port:${port}`)
-})
+app.listen(port, () => {
+  console.log(`server running on port:${port}`);
+});
+
+//syncing the database
+sequelize.sync({ force: false, alter: true }).then(() => {});
